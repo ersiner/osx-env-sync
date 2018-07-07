7006b29bcdc671b43bb1f9f9a13854c45055838e
mexisme <wildjim+dev@kiwinet.org>
2018-07-08 10:25:37 +1200

    Add base doc for osx-sync-env binary

---
+++ osx-env-sync/doc.go
@@ -0,0 +1,8 @@
+/*
+osx-env-sync is part of the github.com/mexisme/osx-env-sync tool-set:
+
+https://github.com/mexisme/osx-env-sync/blob/develop/README.md
+
+  Synchronize OS X environment variables for command line and GUI applications from a single source.
+*/
+package main // import "github.com/mexisme/osx-env-sync/osx-env-sync"
